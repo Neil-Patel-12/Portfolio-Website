03b7fde69bc10366517935062cd7ed0501e866d4
Neil Patel <neilpatel235@gmail.com>
2024-08-08 01:18:04 -0400

    main landing page

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Full-Stack Developer", "Software Engineer", "8 Ball Pool Player"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true,
+});
