d2e18fe98ad1588bdf5b9c911b0073e8d0f1c872
Neil Patel <neilpatel235@gmail.com>
2024-08-11 22:43:35 -0400

    finished up the project tab

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text", {
   strings: ["Full-Stack Developer", "Software Engineer", "8 Ball Pool Player"],
-  typeSpeed: 100,
-  backSpeed: 50,
-  backDelay: 1000,
+  typeSpeed: 60,
+  backSpeed: 30,
+  backDelay: 800,
   loop: true,
 });
